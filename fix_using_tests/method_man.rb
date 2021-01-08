81eea656d91e51140a0b0bac44e68f50cd4ec5a1
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-08 23:12:51 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -3,11 +3,15 @@
 require 'pry'
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{player2}"
+  greet = "Hello #{player1} & #{player2}"
+  puts greet
+  greet
 end
 
 def play_game(player1, player2)
-  "#{player1} is better than #{player2}"
+  brag = "#{player1} is better than #{player2}"
+  puts brag
+  brag
 end
 
 start_game("Tara", "Zoe")
